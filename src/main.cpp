281d1163d388d4970ae4f993997feedcc5afbec0
Caleb Nierman <calebjnierman@proton.me>
2026-09-11 20:25:50 -0500

    created gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,9 @@ int main(void)
 
     auto f
     {
-            std::make_shared<f_gamma>(std::make_shared<f_gamma>(
+            std::make_shared<f_gamma>(
                 std::make_shared<f_var>()
-            ))
+            )
     };
     
 
